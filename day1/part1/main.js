57ac74112d7938ce809bff70e3cc5141abd3254f
KierinC <kierin.chesworth@gmail.com>
2023-01-20 09:13:43 -0700

    day 1 compete

--- day1/part1/main.js
+++ day1/part1/main.js
@@ -1,23 +1,32 @@
 let calories = [];
-let j = 0;
+let temp = 0;
 
 // Read input from file
 const fs = require('fs');
 let array = fs.readFileSync('input.txt').toString().split("\n");
 for(i in array) {
-  // console.log(array[i]);
   array[i] = parseInt(array[i].replace('\r',''));
-  if (!(array[i] >= 0)) {
-    // calories.push(elfTotal);
-    j++;
-    // console.log(j);
+  if ((Number.isNaN(array[i]))) {
+    calories.push(temp);
+    temp = 0;
+  } else {
+    temp += array[i];
   }
-  // elfTotal += array[i];
-  calories[j] += array[i];
+  
+  // calories[j] += array[i];
   // console.log(array[i]);
   // console.log(elfTotal);
 }
+
+  
+// console.log(array);
 console.log(calories);
+calories.sort(compareNumbers);
+console.log(calories[calories.length - 1] + calories[calories.length - 2] + calories[calories.length - 3]);
 // Get sum of groupings of numbers and add them to 'calories' array
 // Sort array to get highest sum first
 // Print calories[0]
+
+function compareNumbers(a, b) {
+  return a - b;
+}
